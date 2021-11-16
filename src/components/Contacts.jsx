4edccaf7e5cdb7a42dcd53042948e49d97d653fc
Katherine-valcarce <katherine.v@live.cl>
2021-11-16 11:56:55 -0300

    correction home and contacts (opacity and hyperlink)

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -8,20 +8,20 @@ const Contacts = () => {
             <img className='imgContacts' src='https://imgur.com/HCACXsc.png' alt='icono contacto'></img>
             <div className='containerIconsContacts'>
                 <div className='containerIconContacts'>
+                <a className='aIcon' href="https://wa.me/56967163650">
                     <img src='https://imgur.com/tbTr0w6.png' alt='icono telefono' className='iconContacts'></img>
-                    <a className='aIcon' href="https://wa.me/56967163650">
                         <p className='textIcon'>+569 6716 3650</p>
                     </a>
                 </div>
                 <div className='containerIconContacts'>
+                <a className='aIcon' href="https://www.linkedin.com/in/katherine-valcarce/"> 
                     <img src='https://imgur.com/ksE8nee.png' alt='icono linkedin' className='iconContacts'></img>
-                    <a className='aIcon' href="https://www.linkedin.com/in/katherine-valcarce/"> 
                     <p className='textIcon'>linkedin.com/in/katherine-valcarce </p>
                     </a>
                 </div>
                 <div className='containerIconContacts'>
+                <a className='aIcon' href="mailto:katherine.v@live.cl">
                     <img img src='https://imgur.com/r8qFUE3.png' alt='icono correo' className='iconContacts'></img>
-                    <a className='aIcon' href="mailto:katherine.v@live.cl">
                         <p className='textIcon'> Katherine.v@live.cl </p>
                     </a>
                 </div>
