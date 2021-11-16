99927cd480957f7e4970abe7cf2f19ee76ad8aa8
Katherine-valcarce <katherine.v@live.cl>
2021-11-16 10:20:35 -0300

    deploy Firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDjxCvO3KAKdNVuBpaVJvFxUfzrrvPVSfE",
+  authDomain: "portfolio-katherinev.firebaseapp.com",
+  projectId: "portfolio-katherinev",
+  storageBucket: "portfolio-katherinev.appspot.com",
+  messagingSenderId: "711658830444",
+  appId: "1:711658830444:web:c3273ca93b38c471733649"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
